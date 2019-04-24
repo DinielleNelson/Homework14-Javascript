3a6c1a3d5eeac10d6aabb9a14854dd7a721729d5
Dinielle <Dinielle.Nelson@gmail.com>
2019-04-24 18:53:11 -0400

    should have committed yesterday. remaining: clearing previous table, #2 of README

--- static/js/app.js
+++ static/js/app.js
@@ -1,16 +1,17 @@
-// from data.js
-var tableData = data;
-table_section = d3.select("tbody")
+// // from data.js
+// var tableData1 = data; 
 
-// YOUR CODE HERE!
+// Maintain a copy of the data so that modifications are not made to the existing data
+var tableData = data.map(dict => dict);
 
-tableData.forEach(function(print){
-    console.log(print.city)
-});
+var table_section = d3.select("tbody");
 
-tableData.forEach(function (Sighting){
+
+// Create function to be used again elsewhere
+function populateTable(Sighting){
+
+
     // possible if statement to add only new entries
-    // dict_value1 = dict.map(row => row.city)
     row_fill = table_section.append("tr")
     row_fill.append("td").text(Sighting.datetime)
     row_fill.append("td").text(Sighting.city)
@@ -19,32 +20,46 @@ tableData.forEach(function (Sighting){
     row_fill.append("td").text(Sighting.shape)
     row_fill.append("td").text(Sighting.durationMinutes)
     row_fill.append("td").text(Sighting.comments)
+};
 
+// 1. * Using the UFO dataset provided in the form of an array of JavaScript objects, write code that appends a table to your web page and 
+// then adds new rows of data for each UFO sighting
 
+tableData.forEach(populateTable);
 
-    // create row which equals one dictionary
-    // row_fill = d3.select("tbody").append("row")
+// Use a date form in your HTML document and write JavaScript code that will listen for events and search through the `date/time` column to 
+// find rows that match user input.
 
-    // // get cell value from first dictionary
-    // dict_value1 = tableData.map(row => row.datetime)
-    // console.log(dict_value1)
-    // dict_value2 = dict_entry.map(row => row.city)
-    // dict_value3 = dict_entry.map(row => row.state)
-    // dict_value4 = dict_entry.map(row => row.country)
-    // dict_value5 = dict_entry.map(row => row.shape)
-    // dict_value6 = dict_entry.map(row => row.durationMinutes)
-    // dict_value7 = dict_entry.map(row => row.comments)
+// TO DO  Reset button
 
-    // // cycle through dictionary
-    // row_fill.append("td").text(dict_value1)
-    // row_fill.append("td").text(dict_value2)
-    // row_fill.append("td").text(dict_value3)
-    // row_fill.append("td").text(dict_value4)
-    // row_fill.append("td").text(dict_value5)
-    // row_fill.append("td").text(dict_value6)
-    // row_fill.append("td").text(dict_value7)
+var submitButton = d3.select("#filter-btn");
+
+submitButton.on("click", function() {
+    d3.event.preventDefault();
+  
+    var inputValue1 = d3.select("#datetime").property("value");
+    var inputValue2 = d3.select("#city").property("value");
+    var inputValue3 = d3.select("#state").property("value");
+    var inputValue4 = d3.select("#country").property("value");
+    var inputValue5 = d3.select("#shape").property("value");
+
+    // clear all previous table data entries
+    d3.select("tbody>td").remove();
+    d3.select("tbody>tr").remove();
+
+    filteredTable = tableData.filter(each => 
+        (each.datetime === inputValue1 || each.datetime === null )&&
+        // each.city === inputValue2 &&
+        // each.state === inputValue3 &&
+        // each.country === inputValue4 &&
+        (each.shape === inputValue5 || each.shape === null) );
+    // test that search is working
+    console.log(filteredTable);
+
+    // return the data back to the table/html
+    filteredTable.forEach(populateTable);
+
 });
 
-// run function
-// return addSighting(tableData){
-     
+// SELF BONUS:: A form to enter new sightings which update the table upon enter
+// When adding new entries, remember to append to the original data set
